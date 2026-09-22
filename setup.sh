8b231d7e434143b69b4e766050a41a9cf6d5642c
txiuqw4 <43464905+txiuqw4@users.noreply.github.com>
2026-09-22 03:03:32 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,11 +16,6 @@ rm -f ForgeMiner-1.8.1-linux.tar.gz
 
 echo "=== Starting ForgeMiner ==="
 
-nohup /opt/forge \
-  --algorithm pearlhash \
-  --pool prl-us.kryptex.network:7048 \
-  --wallet krxY96RPRD \
-  --worker "$HOSTNAME" \
-  > /opt/forge.log 2>&1 &
+nohup /opt/forge --algorithm pearlhash --pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$HOSTNAME" > /opt/forge.log 2>&1 &
 
 wait
