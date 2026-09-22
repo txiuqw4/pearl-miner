69009d4fd5f8d3bd39ea886cc4e5367db2004017
txiuqw4 <43464905+txiuqw4@users.noreply.github.com>
2026-09-22 05:34:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,18 +3,11 @@ set -e
 
 WORKER="${1:-worker-$(od -An -N2 -tu2 /dev/urandom | tr -d ' ')}"
 
-echo "=== Installing ForgeMiner 1.8.1 ==="
-
 cd /opt
 
-curl -L -o ForgeMiner-1.8.1-linux.tar.gz \
-  https://github.com/0xHashRaptor/ForgeMiner/releases/download/v1.8.1/ForgeMiner-1.8.1-linux.tar.gz
-
+curl -L -o ForgeMiner-1.8.1-linux.tar.gz https://github.com/0xHashRaptor/ForgeMiner/releases/download/v1.8.1/ForgeMiner-1.8.1-linux.tar.gz
 tar -xzf ForgeMiner-1.8.1-linux.tar.gz
 chmod +x forge
 rm -f ForgeMiner-1.8.1-linux.tar.gz
 
-echo "=== Starting ForgeMiner ==="
-echo "Worker: $WORKER"
-
-nohup /opt/forge --algorithm pearlhash --pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$WORKER" > /opt/forge.log 2>&1 &
+exec /opt/forge --algorithm pearlhash --pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$WORKER"
