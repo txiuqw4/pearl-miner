3a55e0bae51be5de19ca325a4421168c0054651b
txiuqw4 <43464905+txiuqw4@users.noreply.github.com>
2026-09-23 06:49:26 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,4 +16,4 @@ cd /opt/SRBMiner-Multi-3-6-9/
 
 chmod +x SRBMiner-MULTI
 
-exec /opt/SRBMiner-Multi-3-6-9/SRBMiner-MULTI --disable-cpu --algorithm pearlhash -pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$WORKER" --gpu-coffset0 200 --gpu-cclock0 2490 --gpu-mclock0 7001 --gpu-plimit0 575
+exec /opt/SRBMiner-Multi-3-6-9/SRBMiner-MULTI --disable-cpu --algorithm pearlhash -pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$WORKER"
