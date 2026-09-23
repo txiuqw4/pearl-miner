a57a54800339d896f1ddfb40b715165078cef77a
txiuqw4 <43464905+txiuqw4@users.noreply.github.com>
2026-09-23 06:26:41 +0000

    using srb miner

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ WORKER="${1:-worker-$(od -An -N2 -tu2 /dev/urandom | tr -d ' ')}"
 
 cd /opt
 
-curl -L -o ForgeMiner-1.8.1-linux.tar.gz https://github.com/0xHashRaptor/ForgeMiner/releases/download/v1.8.1/ForgeMiner-1.8.1-linux.tar.gz
-tar -xzf ForgeMiner-1.8.1-linux.tar.gz
-chmod +x forge
-rm -f ForgeMiner-1.8.1-linux.tar.gz
+curl -L -o SRBMiner-Multi-3-6-9-Linux.tar.gz \
+  https://github.com/doktor83/SRBMiner-Multi/releases/download/3.6.9/SRBMiner-Multi-3-6-9-Linux.tar.gz
 
-exec /opt/forge --algorithm pearlhash --pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$WORKER"
+tar -xzf SRBMiner-Multi-3-6-9-Linux.tar.gz
+
+chmod +x SRBMiner-MULTI
+
+rm -f SRBMiner-Multi-3-6-9-Linux.tar.gz
+
+exec /opt/SRBMiner-MULTI --disable-cpu --algorithm pearlhash -pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$WORKER" --gpu-coffset0 200 --gpu-cclock0 2490 --gpu-mclock0 7001 --gpu-plimit0 575
