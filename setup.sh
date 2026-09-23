4fc72967f429637ccc22fb779172c85b0d5f31e0
txiuqw4 <43464905+txiuqw4@users.noreply.github.com>
2026-09-23 06:54:15 +0000

    revert to forgeminer

--- setup.sh
+++ setup.sh
@@ -5,15 +5,9 @@ WORKER="${1:-worker-$(od -An -N2 -tu2 /dev/urandom | tr -d ' ')}"
 
 cd /opt
 
-curl -L -o SRBMiner-Multi-3-6-9-Linux.tar.gz \
-  https://github.com/doktor83/SRBMiner-Multi/releases/download/3.6.9/SRBMiner-Multi-3-6-9-Linux.tar.gz
+curl -L -o ForgeMiner-1.8.1-linux.tar.gz https://github.com/0xHashRaptor/ForgeMiner/releases/download/v1.8.1/ForgeMiner-1.8.1-linux.tar.gz
+tar -xzf ForgeMiner-1.8.1-linux.tar.gz
+chmod +x forge
+rm -f ForgeMiner-1.8.1-linux.tar.gz
 
-tar -xzf SRBMiner-Multi-3-6-9-Linux.tar.gz
-
-rm -f SRBMiner-Multi-3-6-9-Linux.tar.gz
-
-cd /opt/SRBMiner-Multi-3-6-9/
-
-chmod +x SRBMiner-MULTI
-
-exec /opt/SRBMiner-Multi-3-6-9/SRBMiner-MULTI --disable-cpu --algorithm pearlhash -pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$WORKER"
+exec /opt/forge --algorithm pearlhash --pool prl-us.kryptex.network:7048 --wallet krxY96RPRD --worker "$WORKER"
