3651356e24a0741ab291245e38113d07dbf25656
txiuqw4 <43464905+txiuqw4@users.noreply.github.com>
2026-09-24 19:10:47 +0700

    Create wildrig_start.sh

---
+++ wildrig_start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+WORKER="${1:-worker-$(od -An -N2 -tu2 /dev/urandom | tr -d ' ')}"
+
+cd /opt
+
+curl -fL -o wildrig-multi-linux-0.51.2.tar.gz \
+  https://github.com/andru-kun/wildrig-multi/releases/download/0.51.2/wildrig-multi-linux-0.51.2.tar.gz
+
+tar -xzf wildrig-multi-linux-0.51.2.tar.gz
+rm -f wildrig-multi-linux-0.51.2.tar.gz
+
+chmod +x /opt/wildrig-multi
+
+exec /opt/wildrig-multi \
+  --algo pearlhash \
+  --url pool.pearlhash.xyz:9000 \
+  --user prl1pl76eychyzq97as53t855ryct2dfsgghe3sgrpfdl89ctq06py9gs6qnuc7 \
+  --worker "$WORKER" \
+  >> /opt/prl.log 2>&1
