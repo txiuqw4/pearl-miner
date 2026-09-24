cdc600a2dcebbe2e03a110534cc068a8ed738c88
txiuqw4 <43464905+txiuqw4@users.noreply.github.com>
2026-09-24 19:12:54 +0700

    Update wildrig_start.sh

--- wildrig_start.sh
+++ wildrig_start.sh
@@ -16,6 +16,5 @@ chmod +x /opt/wildrig-multi
 exec /opt/wildrig-multi \
   --algo pearlhash \
   --url pool.pearlhash.xyz:9000 \
-  --user prl1pl76eychyzq97as53t855ryct2dfsgghe3sgrpfdl89ctq06py9gs6qnuc7 \
-  --worker "$WORKER" \
+  --user "prl1pl76eychyzq97as53t855ryct2dfsgghe3sgrpfdl89ctq06py9gs6qnuc7.$WORKER" \
   >> /opt/prl.log 2>&1
